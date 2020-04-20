57244c1b166b0c1f54ccc6c36ac3a0e44dc70540
cjmoro <scjmoro@gmail.com>
2020-04-19 23:44:44 -0700

    Add createElement function

--- index.ts
+++ index.ts
@@ -23,3 +23,28 @@ function createTextElement(text: string | number): object {
 		},
 	};
 }
+
+type Didact = {
+	createElement: Function;
+};
+
+const Didact: Didact = {
+	createElement,
+};
+const element = Didact.createElement(
+	"div",
+	{ id: "foo" },
+	Didact.createElement("a", null, "bar"),
+	Didact.createElement("b")
+);
+
+/** @jsx Didact.createElement */
+// const element:any = (
+// 	<div id="foo">
+// 		<a>bar</a>
+// 		<b />
+// 	</div>
+// );
+
+const container = document.getElementById("root") as HTMLElement;
+// ReactDOM.render(element, container)
